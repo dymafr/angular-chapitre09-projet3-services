28a2f12e3a5aceb6cdd94f64bc4082e6f5a4e926
Erwan Vallée <contact@dyma.fr>
2020-03-04 11:52:54 +0100

    done maj ng9

--- cocktails/src/app/app.module.ts
+++ cocktails/src/app/app.module.ts
@@ -1,14 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
-import { CocktailsListComponent } from './cocktail-container/cocktails-list/cocktails-list.component';
-import { CocktailDetailsComponent } from './cocktail-container/cocktail-details/cocktail-details.component';
-import { HeaderComponent } from './header/header.component';
-import { CocktailContainerComponent } from './cocktail-container/cocktail-container.component';
-import { ActiveDirective } from './shared/directives/active.directive';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { AppComponent } from "./app.component";
+import { CocktailsListComponent } from "./cocktail-container/cocktails-list/cocktails-list.component";
+import { CocktailDetailsComponent } from "./cocktail-container/cocktail-details/cocktail-details.component";
+import { HeaderComponent } from "./header/header.component";
+import { CocktailContainerComponent } from "./cocktail-container/cocktail-container.component";
+import { ActiveDirective } from "./shared/directives/active.directive";
 
 @NgModule({
   declarations: [
@@ -19,12 +18,8 @@ import { ActiveDirective } from './shared/directives/active.directive';
     CocktailContainerComponent,
     ActiveDirective
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
